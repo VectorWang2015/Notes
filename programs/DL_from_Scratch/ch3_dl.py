a011a560b22230f660a8ad71f8745bb26a5b9aa5
VectorWang2015 <vectorwang@hotmail.com>
2021-10-02 21:00:04 +0800

    finished optimizer, on trainer

--- programs/DL_from_Scratch/ch3_dl.py
+++ programs/DL_from_Scratch/ch3_dl.py
@@ -232,3 +232,74 @@ class MeanSquaredError(Loss):
 
     def _input_grad(self) -> ndarray:
         return 2.0 * (self.prediction - self.target) / self.prediction.shape[0]
+
+
+class NeuralNetwork(object):
+    def __init__(self, layers: List[Layer],
+            loss: Loss,
+            seed: float = 1):
+        self.layers = layers
+        self.loss = loss
+        self.seed = seed
+
+        if seed:
+            for layer in self.layers:
+                setattr(layer, "seed", self.seed)
+
+    def forward(self, x_batch: ndarray) -> ndarray:
+        x_out = x_batch
+        for layer in self.layers:
+            x_out = layer.forward(x_out)
+
+        return x_out
+
+    def backward(self, loss_grad: ndarray) -> None:
+        grad = loss_grad
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+
+        return None
+
+    def train_batch(self,
+            x_batch: ndarray,
+            y_batch: ndarray) -> float:
+        predictions = self.forward(x_batch)
+        loss = self.loss.forward(predictions, y_batch)
+        self.backward(self.loss.backward())
+
+        return loss
+
+    def params(self):
+        for layer in self.layers:
+            yield from layer.params
+
+    def param_grads(self):
+        for layer in self.layers:
+            yield from layer.param_grads
+
+
+class Optimizer(object):
+    def __init__(self,
+            lr: float = 0.01):
+        # each optimizer has an initial learn-rate
+        self.lr = lr
+
+    def step(self) -> None:
+        pass
+
+
+class SGD(Optimizer):
+    """
+    stochastic gradient descent
+    """
+    def __init__(self,
+            lr: float = 0.01) -> None:
+        super().__init__(lr)
+
+    def step(self):
+        for (param, param_grad) in zip(self.net.params(),
+                self.net.param_grads()):
+            param -= self.lr * param_grad
+
+
+
